71f390e0e40b6cd6acbd18163e691f9c625be519
ylitk <ylitk@DESKTOP-8M799H7>
2024-10-11 08:27:22 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace Задание_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.param1 = new System.Windows.Forms.Label();
+            this.param2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.param3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(21, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(198, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введите параметры:";
+            // 
+            // param1
+            // 
+            this.param1.AutoSize = true;
+            this.param1.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.param1.Location = new System.Drawing.Point(21, 46);
+            this.param1.Name = "param1";
+            this.param1.Size = new System.Drawing.Size(97, 20);
+            this.param1.TabIndex = 1;
+            this.param1.Text = "Параметр 1";
+            // 
+            // param2
+            // 
+            this.param2.AutoSize = true;
+            this.param2.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.param2.Location = new System.Drawing.Point(21, 75);
+            this.param2.Name = "param2";
+            this.param2.Size = new System.Drawing.Size(97, 20);
+            this.param2.TabIndex = 2;
+            this.param2.Text = "Параметр 2";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(152, 44);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(152, 75);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Круг",
+            "Прямоугольник",
+            "Треугольник"});
+            this.comboBox1.Location = new System.Drawing.Point(19, 189);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(356, 24);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(15, 154);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(176, 23);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Выберите фигуру:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(19, 228);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(172, 62);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Вычислить площадь";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(208, 228);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(167, 62);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Вычислить периметр";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Italic | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(18, 307);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 22);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Площадь:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Italic | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(204, 307);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(106, 22);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Периметр:";
+            // 
+            // param3
+            // 
+            this.param3.AutoSize = true;
+            this.param3.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.param3.Location = new System.Drawing.Point(21, 106);
+            this.param3.Name = "param3";
+            this.param3.Size = new System.Drawing.Size(97, 20);
+            this.param3.TabIndex = 11;
+            this.param3.Text = "Параметр 3";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(152, 106);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(392, 382);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.param3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.param2);
+            this.Controls.Add(this.param1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label param1;
+        private System.Windows.Forms.Label param2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label param3;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Задание_1
+{
+    // Класс формы Form1 должен быть первым
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            // Прячем label5 и label6 при загрузке формы
+            label5.Visible = false;
+            label6.Visible = false;
+            // Обработчики событий для кнопок
+            button1.Click += new EventHandler(this.button1_Click);
+            button2.Click += new EventHandler(this.button2_Click);
+        }
+        // Метод для обработки нажатия кнопки "Вычислить площадь"
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IShape shape = CreateShapeFromInput();
+            if (shape != null)
+            {
+                double area = shape.CalculateArea();
+                label5.Text = $"Площадь: {area:F2}";
+                label5.Visible = true;
+            }
+        }
+        // Метод для обработки нажатия кнопки "Вычислить периметр"
+        private void button2_Click(object sender, EventArgs e)
+        {
+            IShape shape = CreateShapeFromInput();
+            if (shape != null)
+            {
+                double perimeter = shape.CalculatePerimeter();
+                label6.Text = $"Периметр: {perimeter:F2}";
+                label6.Visible = true;
+            }
+        }
+        // Метод для создания объекта фигуры на основе выбора пользователя
+        private IShape CreateShapeFromInput()
+        {
+            double param1, param2, param3;
+            // Проверяем корректность ввода всех трех параметров
+            if (!double.TryParse(textBox1.Text, out param1) ||
+                !double.TryParse(textBox2.Text, out param2) ||
+                !double.TryParse(textBox3.Text, out param3))
+            {
+                MessageBox.Show("Введите корректные числовые значения для параметров!");
+                return null;
+            }
+            string selectedShape = comboBox1.SelectedItem?.ToString();
+            switch (selectedShape)
+            {
+                case "Круг":
+                    return new Circle(param1); // param1 - радиус
+                case "Прямоугольник":
+                    return new Rectangle(param1, param2); // param1 - ширина, param2 - высота
+                case "Треугольник":
+                    return new Triangle(param1, param2, param3); // param1, param2 и param3 - три стороны
+                default:
+                    MessageBox.Show("Выберите фигуру из списка!");
+                    return null;
+            }
+        }
+    }
+    // Интерфейс для вычисления площади и периметра
+    public interface IShape
+    {
+        double CalculateArea();
+        double CalculatePerimeter();
+    }
+    // Класс для круга
+    public class Circle : IShape
+    {
+        public double Radius { get; set; }
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+        public double CalculateArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+        public double CalculatePerimeter()
+        {
+            return 2 * Math.PI * Radius;
+        }
+    }
+    // Класс для прямоугольника
+    public class Rectangle : IShape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+        public double CalculateArea()
+        {
+            return Width * Height;
+        }
+        public double CalculatePerimeter()
+        {
+            return 2 * (Width + Height);
+        }
+    }
+    // Класс для треугольника
+    public class Triangle : IShape
+    {
+        public double SideA { get; set; }
+        public double SideB { get; set; }
+        public double SideC { get; set; }
+        public Triangle(double sideA, double sideB, double sideC)
+        {
+            SideA = sideA;
+            SideB = sideB;
+            SideC = sideC;
+        }
+        public double CalculateArea()
+        {
+            double semiPerimeter = CalculatePerimeter() / 2;
+            return Math.Sqrt(semiPerimeter * (semiPerimeter - SideA) * (semiPerimeter - SideB) * (semiPerimeter - SideC));
+        }
+        public double CalculatePerimeter()
+        {
+            return SideA + SideB + SideC;
+        }
+    }
+}
